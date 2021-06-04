076e4315b138df3a75c940b67fc60e82ddead436
sheldoncross <sheldoncrosss@gmail.com>
2021-06-03 20:46:24 -0400

    FEAT: Dark Mode Toggle

---
+++ res/scripts/dark-theme-toggle.js
@@ -0,0 +1,20 @@
+ 
+const btn = document.querySelector(".theme-toggle");
+const theme = document.querySelector("#theme-link");
+const icon = document.querySelector(".theme-toggle-icon");
+const githubLogo = document.querySelector("#github-logo");
+
+
+btn.addEventListener("click", function() {
+  if (theme.getAttribute("href") == "res/scss/light-theme.css") {
+    theme.href = "res/scss/dark-theme.css";
+    btn.classList = "theme-toggle btn btn-dark right m-2";
+    icon.src = "res/assets/white-sun.png";
+    githubLogo.src = "res/assets/GitHub-Mark/PNG/GitHub-Mark-Light-32px.png";
+  } else {
+    theme.href = "res/scss/light-theme.css";
+    btn.classList = "theme-toggle btn btn-light right m-2";
+    icon.src = "res/assets/sun.png";
+    githubLogo.src = "res/assets/GitHub-Mark/PNG/GitHub-Mark-32px.png";
+  }
+});
