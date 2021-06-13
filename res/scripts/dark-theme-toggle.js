95ad24c2a843335fe3f156de631697ad41669e05
sheldoncross <sheldoncrosss@gmail.com>
2021-06-13 12:57:49 -0400

    FIX: Dark mode persistent on projects page FEAT: Added Links to the bottom of the project page

--- res/scripts/dark-theme-toggle.js
+++ res/scripts/dark-theme-toggle.js
@@ -3,10 +3,9 @@ const btn = document.querySelector(".theme-toggle");
 const theme = document.querySelector("#theme-link");
 const icon = document.querySelector(".theme-toggle-icon");
 const githubLogo = document.querySelector("#github-logo");
-const storedTheme = localStorage.getItem("theme");
+var storedTheme = localStorage.getItem("theme");
 
 if (storedTheme == "dark") {
-  document.body.classList.add("dark-theme");
   theme.href = "res/scss/dark-theme.css";
   btn.classList = "theme-toggle btn btn-dark right m-2";
 }
@@ -17,18 +16,14 @@ btn.addEventListener("click", function() {
     btn.classList = "theme-toggle btn btn-dark right m-2";
     icon.src = "res/assets/white-sun.png";
     githubLogo.src = "res/assets/GitHub-Mark/PNG/GitHub-Mark-Light-32px.png";
+    storedTheme = "dark";
   } else {
     theme.href = "res/scss/light-theme.css";
     btn.classList = "theme-toggle btn btn-light right m-2";
     icon.src = "res/assets/sun.png";
     githubLogo.src = "res/assets/GitHub-Mark/PNG/GitHub-Mark-32px.png";
+    storedTheme = "light";
   }
-
-  document.body.classList.toggle("dark-theme");
-
-  let storedTheme = "light";
-  if (document.body.classList.contains("dark-theme")) {
-    storedTheme = "dark";
-  }
+  
   localStorage.setItem("theme", storedTheme);
 });
